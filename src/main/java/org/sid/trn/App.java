fb1285029039517329e439522a0040fbd7ca24ed
root <root@ip-172-31-26-164.us-west-2.compute.internal>
2016-07-26 16:28:02 -0400

    hook first try

--- src/main/java/org/sid/trn/App.java
+++ src/main/java/org/sid/trn/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World! This is Sid!" );
+        System.out.println( "Hello World! This is Sid!1" );
     }
 }
